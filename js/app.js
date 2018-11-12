9192ff4626683495df0642f0033d930091803f72
Shahlo <33292441+sseidmed@users.noreply.github.com>
2018-11-12 12:09:52 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,142 @@
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y + 55;
+    this.speed = speed;
+    this.step = 101;
+    this.boundary = this.step * 5;
+    this.resetPos = -this.step;
+    this.sprite = 'images/Rock.png';
+
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    if(this.x < this.boundary) {
+      this.x += this.speed*dt;
+    }
+    else {
+      this.x = this.resetPos;
+    }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+let score = document.querySelector(".score");
+let scoreNum = 0;
+
+class Hero {
+  constructor() {
+    this.sprite = 'images/char-cat-girl.png';
+    this.step = 101;
+    this.jump = 83;
+    this.startX = this.step * 2;
+    this.startY = (this.jump * 4) + 55;
+    this.x = this.startX;
+    this.y = this.startY;
+    this.victory = false;
+  }
+
+  update() {
+      for(let enemy of allEnemies) {
+        if(this.y === enemy.y && (enemy.x + enemy.step/2 > this.x && enemy.x < this.x + this.step/2)) {
+
+         this.reset();
+     }
+    }
+
+     if(this.y < 55) {
+               scoreNum = scoreNum + 1;
+               score.textContent =  Math.floor(scoreNum / 60);
+             	 setTimeout(this.reset.bind(this), 1000);
+                this.victory = true;
+
+        }
+    }
+
+
+
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+//  @param {string} input - Direction to Travel
+  handleInput(input) {
+    switch(input) {
+      case 'left':
+       if(this.x > 0) {
+       this.x -= this.step;
+     }
+       break;
+      case 'up':
+       if(this.y > 0) {
+       this.y -= this.jump;
+     }
+       break;
+      case 'right':
+       if(this.x < this.step * 4) {
+       this.x += this.step;
+     }
+       break;
+      case 'down':
+       if(this.y < this.jump * 4) {
+       this.y += this.jump;
+     }
+       break;
+    }
+  }
+
+  reset() {
+   	this.x = this.startX;
+   	this.y = this.startY;
+   }
+  }
+
+
+
+const player = new Hero();
+const bug1 = new Enemy(-101, 0, 200);
+const bug2 = new Enemy(-101, 83, 250);
+const bug3 = new Enemy(-101, 166, 270);
+const bug4 = new Enemy((-101*3), 166, 170)
+const allEnemies = [];
+allEnemies.push(bug1, bug2, bug3, bug4);
+console.log(allEnemies);
+console.log(player);
+
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+//var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
